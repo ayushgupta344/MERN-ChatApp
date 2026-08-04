a92e578329d67b1fdb62ba4dcd3db1387ffc8a0d
ayushgupta344 <ayushgupta5234@gmail.com>
2026-08-04 23:45:48 +0530

    Bug fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
 import fs from "fs";
 import path from "path";
+import job from "./lib/cron.js";
 dotenv.config();
 const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL;
