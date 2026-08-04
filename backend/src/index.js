3d9ace2fe950a9ac5d293525d49245ae2bbf827e
ayushgupta344 <ayushgupta5234@gmail.com>
2026-08-05 00:13:33 +0530

    Added CLerk WebHook

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,16 @@ import cors from "cors";
 import fs from "fs";
 import path from "path";
 import job from "./lib/cron.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
 dotenv.config();
 const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL;
 const publicDir = path.join(process.cwd(), "public");
+app.use(
+  "/api/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhook,
+);
 app.use(express.json());
 app.use(cors(
   {origin:process.env.FRONTEND_URL,
