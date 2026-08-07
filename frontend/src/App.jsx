418b031f44b59c8ba4aceb33a8215a154eeff80d
ayushgupta344 <ayushgupta5234@gmail.com>
2026-08-08 00:13:00 +0530

    feat: integrate axios for API calls and add socket.io for real-time features; implement authentication store with Zustand

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,23 @@ import { useAuth } from "@clerk/react";
 import  ChatPage  from "./pages/ChatPage";
 import AuthPage  from "./pages/AuthPage";
 import PageLoader from "./components/PageLoader";
-
+import { useAuthStore } from "./store/useAuthStore";
+import { Toast } from "react-hot-toast";
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
-  if(!isLoaded) {
-    return <PageLoader/>
-  }
+   const clearAuth = useAuthStore((state) => state.clearAuth);
+   const checkAuth = useAuthStore((state) => state.checkAuth);
+   const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+   useEffect(() => {
+     if (!isLoaded) return;
+
+     if (isSignedIn) checkAuth();
+     else clearAuth();
+   }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+   if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
+
   return (
     <ThemeProvider>
       <WallpaperProvider>
@@ -26,6 +37,7 @@ function App() {
             element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
           />
         </Routes>
+        <Toast/>
       </WallpaperProvider>
     </ThemeProvider>
   );
